627ce6f61c7ebf0d9f65456c30d78595fdce1e37
Jaime J Cobo <jaimecobo@outlook.com>
2021-09-29 18:45:52 -0600

    marketcafe24 09-29-21 -- 08

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -104,18 +104,27 @@ import suscribete from '../images/edited/Subscribe01.png'
                         </div> */}
 
                     </form>
+
+                    <div className="d-flex justify-content-center" id="recaptchaSubscribe">
+                    <ReCAPTCHA
+                        // sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+                        sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+                        // sitekey={process.env.REACT_APP_RECAPTCHA_SECRET_KEY}
+                        onChange={handleOnChange}
+                    />
+                </div>
                     
                 </div>
                 {/* <div id="flag_rigth"></div> */}
             
-                <div className="d-flex justify-content-center" id="recaptchaSubscribe">
+                {/* <div className="d-flex justify-content-center" id="recaptchaSubscribe">
                     <ReCAPTCHA
                         // sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
                         // sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
                         sitekey={process.env.REACT_APP_RECAPTCHA_SECRET_KEY}
                         onChange={handleOnChange}
                     />
-                </div>
+                </div> */}
                 
             </div>
 
