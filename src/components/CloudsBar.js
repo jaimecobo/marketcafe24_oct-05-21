88947edd1c163a9607cb3c7ff9c6938f7914dc01
Jaime J Cobo <jaimecobo@outlook.com>
2021-10-08 21:52:14 -0600

    improved file size 10-05-21 002

--- src/components/CloudsBar.js
+++ src/components/CloudsBar.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import '../css/CloudsBar.css';
 
-
-import clouds from '../images/Clouds_bar.jpg';
-import decaf_owl from '../images/caffeine owl/Decaf_owl.png'
-import HalfCaf_owl from '../images/caffeine owl/Half-caf_owl.png'
-import Regular_owl from '../images/caffeine owl/Regular_owl.png'
-import Irish_coffee_owl from '../images/caffeine owl/Irish_coffee_owl.png'
-import Expresso_owl from '../images/caffeine owl/Expresso_owl.png'
-import Double_expresso_owl from '../images/caffeine owl/Double_expresso_owl.png'
+import clouds from '../images/New_edited/Owls_Caf_img_01.png'
+// import clouds from '../images/Clouds_bar.jpg';
+// import decaf_owl from '../images/caffeine owl/Decaf_owl.png'
+// import HalfCaf_owl from '../images/caffeine owl/Half-caf_owl.png'
+// import Regular_owl from '../images/caffeine owl/Regular_owl.png'
+// import Irish_coffee_owl from '../images/caffeine owl/Irish_coffee_owl.png'
+// import Expresso_owl from '../images/caffeine owl/Expresso_owl.png'
+// import Double_expresso_owl from '../images/caffeine owl/Double_expresso_owl.png'
 
 
 import logo01 from '../images/edited/01_Logo01_3.png'
@@ -22,24 +22,24 @@ class CloudsBar extends Component {
 
             <div className="image-container" id="cloudsBarContainer">
                 <img id="cloudsBar" className="img-fluid" alt="" src={clouds} /> 
-                <span>
+                {/* <span>
                     <img alt="" id="caffeine_owl01" src={decaf_owl}/>
-                </span>
-                <span>
+                </span> */}
+                {/* <span>
                     <img alt="" id="caffeine_owl02" src={HalfCaf_owl}/>
-                </span>
-                <span>
+                </span> */}
+                {/* <span>
                     <img alt="" id="caffeine_owl03" src={Regular_owl}/>
-                </span>
-                <span>
+                </span> */}
+                {/* <span>
                     <img alt="" id="caffeine_owl04" src={Irish_coffee_owl}/>
-                </span>
-                <span>
+                </span> */}
+                {/* <span>
                     <img alt="" id="caffeine_owl05" src={Expresso_owl}/> 
-                </span>
-                <span>
+                </span> */}
+                {/* <span>
                     <img alt="" id="caffeine_owl06" src={Double_expresso_owl}/>  
-                </span>
+                </span> */}
 
             </div>
 
