ef602ab0ce9ac238e9bc8fb4a05a17a9e805d04b
Jaime J Cobo <jaimecobo@outlook.com>
2021-10-08 21:53:08 -0600

    improved file size 10-05-21 007

--- src/components/Torres_img.js
+++ src/components/Torres_img.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import '../css/Torres_img.css';
 import '../css/Dishes.css';
-import torres from '../images/madrid_torres.jpg'
-import services01 from '..//images/Services01.svg'
-import services02 from '..//images/Services02.svg'
+// import torres from '../images/madrid_torres.jpg'
+import torres from '../images/New_edited/Torres_img_01.png'
+// import services01 from '..//images/Services01.svg'
+// import services02 from '..//images/Services02.svg'
 
 
 class Torres_img extends Component {
@@ -14,8 +15,8 @@ class Torres_img extends Component {
             <div class="image-container">
 
                 <img alt="" id="torres_img" src={torres} />
-                <img alt="" id="services01" src={services01}/>
-                <img alt="" id="services02" src={services02}/>
+                {/* <img alt="" id="services01" src={services01}/> */}
+                {/* <img alt="" id="services02" src={services02}/> */}
                 
             </div>
 
